ca19cad4c5f73a1ca3489652f75201cdc42e5e98
s-ko <song.1209@icloud.com>
2023-11-12 14:30:32 +0900

    add: GetUserByEmail()

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -18,3 +18,10 @@ type userRepository struct {
 func NewUserRepository(db *gorm.DB) IUserRepository {
 	return &userRepository{db}
 }
+
+func (ur *userRepository) GetUserByEmail(user *model.User, email string) error {
+	if err := ur.db.Where("email=?", email).First(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
