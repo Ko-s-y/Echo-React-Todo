468f70b6c31c4408f7ae33de2730700f727dbb47
s-ko <song.1209@icloud.com>
2023-11-12 14:34:13 +0900

    add: user_controller, use Echo, go mod tidy

---
+++ controller/user_controller.go
@@ -0,0 +1,21 @@
+package controller
+
+import (
+	"go-rest-api/usecase"
+
+	"github.com/labstack/echo/v4"
+)
+
+type IUserController interface {
+	SignUp(c echo.Context) error
+	LogIn(c echo.Context) error
+	LogOut(c echo.Context) error
+}
+
+type userController struct {
+	uu usecase.IUserUsecase
+}
+
+func NewUserController(uu usecase.IUserUsecase) IUserController {
+	return &userController{uu}
+}
